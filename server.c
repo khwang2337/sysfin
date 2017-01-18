6bc77c954063934edd7bd456c2dfce6e5763f347
khwang1 <jh2933@gmail.com>
2017-01-18 03:02:57 +0000

    commenting

--- server.c
+++ server.c
@@ -59,33 +59,33 @@ void sub_server( int sd ) {
 }
 
 void registerr(int sd, struct character player) {
-  char name[MESSAGE_BUFFER_SIZE];
-  int check = 1;
-  while (check) {
-    write(sd, "send", 5);
-    read(sd, name, MESSAGE_BUFFER_SIZE);
-    if ( checkAV(name) ) {
-      check = 0;
+  char name[MESSAGE_BUFFER_SIZE]; //name buffer
+  int check = 1; //while conditional
+  while (check) { //checks conditional
+    write(sd, "send", 5); //sends message to client telling it to prompt
+    read(sd, name, MESSAGE_BUFFER_SIZE); //read name the client sends
+    if ( checkAV(name) ) { //checks availability
+      check = 0; //if available, end loop
     }
-  }
+  }// else continue asking for name
   
 }
 
 int checkAV(char * name) {
-  char NAME[MESSAGE_BUFFER_SIZE];
-  int pos = 0;
-  FILE *acc = fopen("accounts", "r");
-  char token = getc(acc);
-  if (token != EOF) {
-    NAME[pos++] = token;
-    while (token != EOF); {
-      while (token = getc(acc) != ' ' && token != EOF) NAME[pos++] = token;
-      if (strcmp(name, NAME)) return 0;
-      while (token = getc(acc) != '\n' && token != EOF); 
-      pos = 0;
+  char NAME[MESSAGE_BUFFER_SIZE]; //account name buffer
+  int pos = 0; //pos for char
+  FILE *acc = fopen("accounts", "r"); //open file stream
+  char token = getc(acc); //gets the first char
+  if (token != EOF) { //checks if first char is EOF, if so return 1
+    NAME[pos++] = token; //else put char at start of NAME
+    while (token != EOF); { //conditional for EOF
+      while (token = getc(acc) != ' ' && token != EOF) NAME[pos++] = token; //adds chars into NAME until the first space or EOF (not really expecting an EOF)
+      if (! strcmp(name, NAME)) return 0; //check name if name already exists, return 0
+      while (token = getc(acc) != '\n' && token != EOF); //pushes token past \n or EOF
+      pos = 0; //resets pos
     }
-  }
-  return 1;
+  } 
+  return 1; //either first token is EOF or name is not in file, good to go
 }
 
 
@@ -97,4 +97,6 @@ void process( char * s ) {
   }
 }
 
-char * convert(struct character player);
+//THESE WILL HAVE TO BE USED CRUCIALLY FOR SAVE PROGRESS AND COMMUNICATION
+char * convertS(struct character player); //converts player struct to a string we can right into accounts
+void convertC(struct character player); //converts string to player struct
